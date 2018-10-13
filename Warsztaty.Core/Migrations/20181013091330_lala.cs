0fd7fcfa56455e511dced6691b7f68d158a56496
Mateusz Woda <Mateusz.Woda@unit4.com>
2018-10-13 12:40:21 +0200

    'First'

---
+++ Warsztaty.Core/Migrations/20181013091330_lala.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Warsztaty.Core.Migrations
+{
+    public partial class lala : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Contact");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Contact",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
