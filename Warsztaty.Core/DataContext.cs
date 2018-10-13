f3279964fdb0262827cea7c626d0c5532775bd98
Mateusz Woda <Mateusz.Woda@unit4.com>
2018-10-13 13:16:54 +0200

    'Fix_connection_string'

--- Warsztaty.Core/DataContext.cs
+++ Warsztaty.Core/DataContext.cs
@@ -10,7 +10,7 @@ namespace Warsztaty.Core
         public virtual DbSet<ContactModel> Contact { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CodeAndCloud;Trusted_Connection=true;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:azuresqlunit4.database.windows.net,1433;Initial Catalog=AzureSQLUnit4Warsztaty;Persist Security Info=False;User ID=sql;Password=123QWEasd!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
